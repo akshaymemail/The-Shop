962de5989adbf91a27d3d4bb18f4978cb58080f2
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2021-09-06 13:01:59 +0530

    add dummy data

---
+++ models/products.js
@@ -0,0 +1,12 @@
+class Product {
+    constructor(id, ownerid, title, imageUrl, description, price) {
+        this.id = id,
+        this.ownerid = ownerid,
+        this.title = title,
+        this.imageUrl = imageUrl,
+        this.description = description,
+        this.price = price
+    }
+}
+
+export default Product;
